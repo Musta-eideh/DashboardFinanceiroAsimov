69a533ade17b3c2703819282aeb48bf1edfe0cea
Mustafa Eideh <mustafaeideh100@outlook.com>
2023-11-12 01:08:34 -0300

    Adicionando uma sidebar sem funcionalidade apenas estilo

--- components/sidebar.py
+++ components/sidebar.py
@@ -17,8 +17,37 @@ import pandas as pd
 
 # ========= Layout ========= #
 layout = dbc.Col([
-                
-            ])
+                html.H1("My Budget", className="text-primary"), 
+                html.P("By Asimov", className="text-info"), 
+                html.Hr(), # Quebra de linha 
+            
+        # Seção de Perfil
+                dbc.Button(id='botao_avatar',
+                           children=[html.Img(src='/assets/img_hom.png', id='avatar_change', alt='Avatar', className='perfil_avatar')], style={'background-color': 'transparent', 'border-color': 'transparent'}),
+
+        # Seção NOVO
+                dbc.Row([
+                    dbc.Col([
+                        dbc.Button(color='success', id='open-novo-receita',
+                        children=['+ Receita'])
+                    ], width=6),
+                    dbc.Col([
+                        dbc.Button(color='danger', id='open-novo-despesa',
+                        children=['- Despesa'])
+                    ], width=6)
+                ]),
+
+        # Seção NAV
+                html.Hr(),
+                dbc.Nav(
+                    [
+                        dbc.NavLink("Dashboard", href="/dashboards", active="exact"),
+                        dbc.NavLink("Extratos", href="/extratos", active="exact"),
+                    ], vertical = True, pills = True, id='nav_buttons', style={"margin-bottom": "50px"}
+                ),
+
+
+            ], id="sidebar_completa")
 
 
 
